f9d0fad913caa7590b52ee1bcae7ebaa541d9308
alexelmas1236 <alejandro.acland@hotmail.com>
2025-02-14 11:40:35 -0300

    Add files via upload

---
+++ test_aislado.js
@@ -0,0 +1,13 @@
+// --- test.js ---
+window.addEventListener('load', () => {
+    console.log("test.js cargado");
+
+    // Esperar hasta que la escena esté completamente cargada
+    const scene = game.scene.scenes[0]; // Suponemos que el jugador está en la primera escena
+
+    // Escuchar el evento playerDataUpdate
+    scene.events.on('playerDataUpdate', (data) => {
+        // Mostrar los valores del jugador con números enteros
+        console.log(`X: ${Math.floor(data.x)}, Rot: ${Math.floor(data.rotation)}`);
+    });
+});
